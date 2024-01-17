c74ed5d94e52a76a2a416c0232fb3e7aa4232b3d
Valoeghese <valoeghesemods@gmail.com>
2024-01-17 21:39:32 +1300

    add some main menu functionality

---
+++ main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://level.tscn")
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit()
