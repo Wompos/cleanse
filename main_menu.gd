fa5601bc3609cc7d8549e76dba5ade03d59d972d
Valoeghese <valoeghesemods@gmail.com>
2024-01-17 21:43:57 +1300

    keyboard control

--- main_menu.gd
+++ main_menu.gd
@@ -1,5 +1,10 @@
 extends Control
 
+# Reference: https://www.youtube.com/watch?v=Mx3iyz8AUAE
+
+func _ready():
+	$VBoxContainer/Start.grab_focus()
+	
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://level.tscn")
 
